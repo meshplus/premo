e4561bc1be383c5d995b1efde5783d8987ec4468
lousong <lousong@hyperchain.cn>
2020-10-12 21:07:22 +0800

    fix TestGetReceiptByHash sig not comply

--- tester/bxh_tester/case009_transaction_test.go
+++ tester/bxh_tester/case009_transaction_test.go
@@ -182,8 +182,8 @@ func (suite *Snake) TestGetReceiptByHash() {
 		Timestamp: time.Now().UnixNano(),
 	}
 
-	err := tx.Sign(suite.pk)
-	suite.Require().Nil(err)
+	// err = tx.Sign(suite.pk)
+	// suite.Require().Nil(err)
 
 	hash, err := suite.client.SendTransaction(tx, nil)
 	suite.Require().Nil(err)
