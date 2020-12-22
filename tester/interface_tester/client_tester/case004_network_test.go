2590dc1226f40aaddfc212c47de9cd2c259de3e4
leixiaochen <leixiaochen@hyperchain.cn>
2020-12-22 16:32:34 +0800

    Modify and add the test cases of http-interface
    Modify and add the test cases of http-interface

--- tester/interface_tester/client_tester/case004_network_test.go
+++ tester/interface_tester/client_tester/case004_network_test.go
@@ -9,13 +9,7 @@ func (suite *Snake) TestGetNetwork() {
 	ret, err := parseResponse(data)
 	suite.Require().Nil(err)
 
-	retJson, err := prettyJson(ret)
-	suite.Require().Nil(err)
-	suite.Require().NotNil(retJson)
-	// network info
-	suite.Require().Contains(retJson, "QmQUcDYCtqbpn5Nhaw4FAGxQaSSNvdWfAFcpQT9SPiezbS")
-	suite.Require().Contains(retJson, "QmQW3bFn8XX1t4W14Pmn37bPJUpUVBrBjnPuBZwPog3Qdy")
-	suite.Require().Contains(retJson, "QmXi58fp9ZczF3Z5iz1yXAez3Hy5NYo1R8STHWKEM9XnTL")
-	suite.Require().Contains(retJson, "QmbmD1kzdsxRiawxu7bRrteDgW1ituXupR8GH6E2EUAHY4")
-
+	suite.Require().Contains(ret, "pid")
+	suite.Require().Contains(ret, "account")
+	suite.Require().Contains(ret, "hosts")
 }
--- tester/interface_tester/client_tester/case005_receipt_test.go
+++ tester/interface_tester/client_tester/case005_receipt_test.go
@@ -4,7 +4,7 @@ import (
 	"time"
 )
 
-func (suite Snake) TestGetReceiptIsTrue() {
+func (suite Snake) TestGetReceipt() {
 	hash, err := suite.sendInterchain()
 	suite.Require().Nil(err)
 
@@ -15,23 +15,27 @@ func (suite Snake) TestGetReceiptIsTrue() {
 
 	data, err := httpGet(url)
 	suite.Require().Nil(err)
+	suite.Require().Contains(string(data), "SUCCESS")
 	suite.Require().NotContains(string(data), "error")
 }
 
-func (suite Snake) TestGetReceiptIsFalse() {
+func (suite Snake) TestGetReceiptWithNonexistent() {
+	wrongHash := "0x0000000000000000000000000000000012345678900000000000000000000000"
 
-	hash, err := suite.sendInterchain()
-	suite.Require().Nil(err)
-
-	//wait for bitxhub
-	time.Sleep(time.Second * 3)
-	hashByte := []byte(hash)
-	hashByte[len(hash)-1] = hashByte[len(hash)-1] + 1
-
-	url := getURL("receipt/" + string(hashByte))
-	suite.Require().Nil(err)
+	url := getURL("receipt/" + wrongHash)
 
 	data, err := httpGet(url)
 	suite.Require().Nil(err)
 	suite.Require().Contains(string(data), "error")
+	suite.Require().Contains(string(data), "not found in DB")
+}
+func (suite Snake) TestGetReceiptWithInvalidFormat() {
+	wrongHash := "0x0000000000000000000000000000000012345678900000000000000000000000"
+
+	url := getURL("receipt/" + wrongHash + "123!@#")
+
+	data, err := httpGet(url)
+	suite.Require().Nil(err)
+	suite.Require().Contains(string(data), "error")
+	suite.Require().Contains(string(data), "invalid format of receipt hash for querying receipt")
 }
