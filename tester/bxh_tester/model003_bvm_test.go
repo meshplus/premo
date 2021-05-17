546824b9ffde57b0192c045acebeaa589600a997
zhuweijie <760297795@qq.com>
2021-05-17 15:45:37 +0800

    one suite to multiple suite

--- tester/bxh_tester/model003_bvm_test.go
+++ tester/bxh_tester/model003_bvm_test.go
@@ -7,8 +7,12 @@ import (
 	"github.com/meshplus/bitxhub-model/pb"
 )
 
+type Model3 struct {
+	*Snake
+}
+
 //tc:调用store合约，set 10M数据，交易回执显示失败
-func (suite *Snake) Test0301_Set10MData() {
+func (suite *Model3) Test0301_Set10MData() {
 	rand10MBytes := make([]byte, 1<<23+1<<21)
 	_, err := rand.Read(rand10MBytes)
 	suite.Require().Nil(err)
@@ -19,7 +23,7 @@ func (suite *Snake) Test0301_Set10MData() {
 }
 
 //tc:调用store合约，get的key为空，交易回执显示失败
-func (suite *Snake) Test0302_GetEmptyKey() {
+func (suite *Model3) Test0302_GetEmptyKey() {
 	receipt, err := suite.client.InvokeBVMContract(constant.StoreContractAddr.Address(), "Get", nil, pb.String("key_for_not_exist"))
 	suite.Require().Nil(err)
 	suite.Require().Equal(pb.Receipt_FAILED, receipt.Status)
@@ -27,14 +31,14 @@ func (suite *Snake) Test0302_GetEmptyKey() {
 }
 
 //tc:调用store合约，set的key为空，交易回执显示失败
-func (suite *Snake) Test0303_SetEmptyKey() {
+func (suite *Model3) Test0303_SetEmptyKey() {
 	receipt, err := suite.client.InvokeBVMContract(constant.StoreContractAddr.Address(), "Set", nil, pb.String(""), pb.String("value_for_empty"))
 	suite.Require().Nil(err)
 	suite.Require().True(receipt.IsSuccess())
 }
 
 //tc:调用store合约，set的value为空，交易回执显示失败
-func (suite *Snake) Test0304_SetEmptyValue() {
+func (suite *Model3) Test0304_SetEmptyValue() {
 	receipt, err := suite.client.InvokeBVMContract(constant.StoreContractAddr.Address(), "Set", nil, pb.String("key_for_empty"), pb.String(""))
 	suite.Require().Nil(err)
 	suite.Require().True(receipt.IsSuccess())
@@ -42,7 +46,7 @@ func (suite *Snake) Test0304_SetEmptyValue() {
 
 //tc:调用store合约，set （a，b），交易回执显示成功
 //tc:调用store合约，get（a），交易回执状态显示成功并且回执数据为b
-func (suite *Snake) Test0305_SetAndGetNormal() {
+func (suite *Model3) Test0305_SetAndGetNormal() {
 	normalKey := "key_for_normal"
 	normalValue := "value_for_normal"
 	receipt1, err := suite.client.InvokeBVMContract(constant.StoreContractAddr.Address(), "Set", nil, pb.String(normalKey), pb.String(normalValue))
--- tester/bxh_tester/model004_xvm_test.go
+++ tester/bxh_tester/model004_xvm_test.go
@@ -1,7 +1,6 @@
 package bxh_tester
 
 import (
-	"fmt"
 	"io/ioutil"
 
 	"github.com/meshplus/bitxhub-kit/types"
@@ -9,19 +8,21 @@ import (
 	rpcx "github.com/meshplus/go-bitxhub-client"
 )
 
-func (suite *Snake) Test0411_LegerSet() {
+type Model4 struct {
+	*Snake
+}
+
+func (suite *Model4) Test0401_LegerSet() {
 	address := suite.deployLedgerContract()
 
 	res, err := suite.client.InvokeXVMContract(address, "state_test_set", nil, rpcx.String("Alice"), rpcx.String("111"))
 	suite.Require().Nil(err)
-	fmt.Println(string(res.Ret))
 	suite.Require().Equal(pb.Receipt_SUCCESS, res.Status)
 	suite.Require().Equal("1", string(res.Ret))
 }
 
-func (suite Snake) Test0412_LegerSetWithValueLoss() {
+func (suite *Model4) Test0402_LegerSetWithValueLoss() {
 	address := suite.deployLedgerContract()
-	fmt.Println(address.String())
 
 	res, err := suite.client.InvokeXVMContract(address, "state_test_set", nil, rpcx.String("Alice"))
 	suite.Require().Nil(err)
@@ -29,7 +30,7 @@ func (suite Snake) Test0412_LegerSetWithValueLoss() {
 	suite.Require().Contains(string(res.Ret), "Missing 1 argument(s)")
 }
 
-func (suite Snake) Test0413_LegerSetWithKVLoss() {
+func (suite *Model4) Test0403_LegerSetWithKVLoss() {
 	address := suite.deployLedgerContract()
 
 	res, err := suite.client.InvokeXVMContract(address, "state_test_set", nil)
@@ -38,7 +39,7 @@ func (suite Snake) Test0413_LegerSetWithKVLoss() {
 	suite.Require().Contains(string(res.Ret), "Missing 2 argument(s)")
 }
 
-func (suite Snake) Test0414_LegerSetWithErrorMethod() {
+func (suite *Model4) Test0404_LegerSetWithErrorMethod() {
 	address := suite.deployLedgerContract()
 
 	res, err := suite.client.InvokeXVMContract(address, "state_test_set111", nil, rpcx.String("Alice"))
@@ -47,7 +48,7 @@ func (suite Snake) Test0414_LegerSetWithErrorMethod() {
 	suite.Require().Contains(string(res.Ret), "wrong rule contract")
 }
 
-func (suite *Snake) Test0415_LegerSetRepeat() {
+func (suite *Model4) Test0405_LegerSetRepeat() {
 	address := suite.deployLedgerContract()
 
 	res, err := suite.client.InvokeXVMContract(address, "state_test_set", nil, rpcx.String("Alice"), rpcx.String("111"))
@@ -61,7 +62,7 @@ func (suite *Snake) Test0415_LegerSetRepeat() {
 	suite.Require().Equal("1", string(res.Ret))
 }
 
-func (suite *Snake) Test0416_LegerGetAliceWithoutSet() {
+func (suite *Model4) Test0406_LegerGetAliceWithoutSet() {
 	address := suite.deployLedgerContract()
 
 	res, err := suite.client.InvokeXVMContract(address, "state_test_get", nil, rpcx.String("Alice"), rpcx.String("111"))
@@ -70,7 +71,7 @@ func (suite *Snake) Test0416_LegerGetAliceWithoutSet() {
 	suite.Require().Contains(string(res.Ret), "Failed to call the `state_test_get` exported function.")
 }
 
-func (suite *Snake) Test0417_GetNilWithoutSet() {
+func (suite *Model4) Test0407_GetNilWithoutSet() {
 	address := suite.deployLedgerContract()
 
 	res, err := suite.client.InvokeXVMContract(address, "state_test_get", nil)
@@ -79,7 +80,7 @@ func (suite *Snake) Test0417_GetNilWithoutSet() {
 	suite.Require().Contains(string(res.Ret), "Missing 2 argument(s)")
 }
 
-func (suite *Snake) Test0418_SetAliceGetAlice() {
+func (suite *Model4) Test0408_SetAliceGetAlice() {
 	address := suite.deployLedgerContract()
 
 	res, err := suite.client.InvokeXVMContract(address, "state_test_set", nil, rpcx.String("Alice"), rpcx.String("111"))
@@ -93,7 +94,7 @@ func (suite *Snake) Test0418_SetAliceGetAlice() {
 	suite.Require().Equal("1", string(res.Ret))
 }
 
-func (suite *Snake) Test0419_SetAliceGetBob() {
+func (suite *Model4) Test0409_SetAliceGetBob() {
 	address := suite.deployLedgerContract()
 
 	res, err := suite.client.InvokeXVMContract(address, "state_test_set", nil, rpcx.String("Alice"), rpcx.String("111"))
@@ -107,7 +108,7 @@ func (suite *Snake) Test0419_SetAliceGetBob() {
 	suite.Require().Contains(string(res.Ret), "Failed to call the `state_test_get` exported function.")
 }
 
-func (suite *Snake) Test0420_SetAliceGetNil() {
+func (suite *Model4) Test0410_SetAliceGetNil() {
 	address := suite.deployLedgerContract()
 
 	res, err := suite.client.InvokeXVMContract(address, "state_test_set", nil, rpcx.String("Alice"), rpcx.String("111"))
@@ -121,7 +122,7 @@ func (suite *Snake) Test0420_SetAliceGetNil() {
 	suite.Require().Contains(string(res.Ret), "Missing 2 argument(s)")
 }
 
-func (suite Snake) Test0421_SetAliceGetAliceRepeat() {
+func (suite *Model4) Test0411_SetAliceGetAliceRepeat() {
 	address := suite.deployLedgerContract()
 
 	res, err := suite.client.InvokeXVMContract(address, "state_test_set", nil, rpcx.String("Alice"), rpcx.String("111"))
