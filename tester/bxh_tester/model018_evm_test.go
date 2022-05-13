b9148b27bf742c69334cde703510f75ddf6bf8c5
Ruoxin <liruoxin@hyperchain.cn>
2022-05-13 09:37:50 +0800

    fix: rawTx input type

--- tester/bxh_tester/model018_evm_test.go
+++ tester/bxh_tester/model018_evm_test.go
@@ -2,14 +2,18 @@ package bxh_tester
 
 import (
 	"encoding/json"
-	"fmt"
 	"math/big"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
+	crypto2 "github.com/meshplus/bitxhub-kit/crypto"
+	"github.com/meshplus/bitxhub-kit/crypto/asym"
+	"github.com/meshplus/bitxhub-model/constant"
 	"github.com/meshplus/bitxhub-model/pb"
 	"github.com/meshplus/premo/internal/repo"
 	"github.com/onrik/ethrpc"
@@ -29,9 +33,12 @@ func (suite Model18) Test1801_GetProtocolVersionIsSuccess() {
 
 //tc：获取bitxhub chainID，获取成功
 func (suite Model18) Test1802_GetChainIDIsSuccess() {
-	res, err := suite.client.Call("eth_chainId")
+	resp, err := suite.client.Call("eth_chainId")
 	suite.Require().Nil(err)
-	suite.Require().Equal("0x54c", string(res))
+	var res string
+	err = json.Unmarshal(resp, &res)
+	suite.Require().Nil(err)
+	suite.Require().Equal(hexutil.EncodeUint64(1356), res)
 }
 
 //tc：获取当前节点是否在挖矿，获取成功
@@ -42,10 +49,10 @@ func (suite Model18) Test1803_GetMiningStatusIsSuccess() {
 }
 
 //tc：获取当前节点的算力，获取成功
-func (suite Model18) Test1804_GetHashrateIsSuccess() {
-	hashrate, err := suite.client.EthHashrate()
+func (suite Model18) Test1804_GetHashRateIsSuccess() {
+	hashRate, err := suite.client.EthHashrate()
 	suite.Require().Nil(err)
-	suite.Require().Equal(0, hashrate)
+	suite.Require().Equal(0, hashRate)
 }
 
 //tc：获取当前gas的价格，获取成功
@@ -57,9 +64,12 @@ func (suite Model18) Test1805_GetGasPriceIsSuccess() {
 
 //tc：获取gas上限的建议，获取成功
 func (suite Model18) Test1806_GetMaxPriorityFeePerGasIsSuccess() {
-	res, err := suite.client.Call("eth_maxPriorityFeePerGas")
+	resp, err := suite.client.Call("eth_maxPriorityFeePerGas")
 	suite.Require().Nil(err)
-	suite.Require().Equal("0x0", string(res))
+	var res string
+	err = json.Unmarshal(resp, &res)
+	suite.Require().Nil(err)
+	suite.Require().Equal("0x0", res)
 }
 
 //tc：获取当前的区块高度，获取成功
@@ -87,7 +97,19 @@ func (suite Model18) Test1808_GetBalanceIsSuccess() {
 
 //tc：根据正确的账户地址和正确的关键字获取存储，获取成功
 func (suite Model18) Test1809_GetStorageAtIsSuccess() {
-
+	pk, err := asym.GenerateKeyPair(crypto2.Secp256k1)
+	suite.Require().Nil(err)
+	client := suite.NewClient(pk)
+	res, err := client.InvokeBVMContract(constant.StoreContractAddr.Address(), "Set", nil, pb.String("key"), pb.String("value"))
+	suite.Require().Nil(err)
+	suite.Require().Equal(pb.Receipt_SUCCESS, res.Status)
+	data, err := suite.client.Call("eth_getStorageAt", constant.StoreContractAddr.String(), "key", "latest")
+	suite.Require().Nil(err)
+	str1 := strings.ReplaceAll(string(data), "\"", "")
+	bytes, err := hexutil.Decode(str1)
+	suite.Require().Nil(err)
+	str2 := strings.ReplaceAll(string(bytes), "\"", "")
+	suite.Require().Equal("value", str2)
 }
 
 //tc：根据正确的账户地址获取当前账户发生的交易数量，获取成功
@@ -148,17 +170,16 @@ func (suite Model18) Test1814_GetBlockTransactionCountByNumberWithNoExistHeightI
 func (suite Model18) Test1815_GetCodeIsSuccess() {
 	address, err := suite.DeploySimpleRule()
 	suite.Require().Nil(err)
-	fmt.Println(address)
 	code, err := suite.client.EthGetCode(address, "latest")
 	suite.Require().Nil(err)
-	fmt.Println(code)
+	suite.Require().NotNil(code)
 }
 
-//tc：根据不存在的的合约地址获取合约，获取成功
+//tc：根据不存在的的合约地址获取合约，获取失败
 func (suite Model18) Test1816_GetCodeWithNoExistAddrIsFail() {
 	code, err := suite.client.EthGetCode("0x0000000000000000000000000000000000000000", "latest")
 	suite.Require().Nil(err)
-	fmt.Println(code)
+	suite.Require().Equal("0x", code)
 }
 
 //tc：根据正确的交易hash获取日志，获取成功
@@ -203,7 +224,7 @@ func (suite Model18) Test1819_SendRawTransactionIsSuccess() {
 	signTx, err := types.SignTx(tx, types.NewEIP155Signer(big.NewInt(1356)), privateKey)
 	suite.Require().Nil(err)
 	data, err := signTx.MarshalBinary()
-	rawTx := common.Bytes2Hex(data)
+	rawTx := hexutil.Bytes(data).String()
 
 	hash, err := suite.client.EthSendRawTransaction(rawTx)
 	suite.Require().Nil(err)
@@ -246,7 +267,7 @@ func (suite Model18) Test1820_SendRawTransactionWithEmptyToIsFail() {
 	signTx, err := types.SignTx(tx, types.NewEIP155Signer(big.NewInt(1356)), privateKey)
 	suite.Require().Nil(err)
 	data, err := signTx.MarshalBinary()
-	rawTx := common.Bytes2Hex(data)
+	rawTx := hexutil.Bytes(data).String()
 
 	_, err = suite.client.EthSendRawTransaction(rawTx)
 	suite.Require().NotNil(err)
@@ -276,7 +297,7 @@ func (suite Model18) Test1821_SendRawTransactionWithEmptySignatureIsFail() {
 		Data:     []byte{},
 	})
 	data, err := tx.MarshalBinary()
-	rawTx := common.Bytes2Hex(data)
+	rawTx := hexutil.Bytes(data).String()
 
 	_, err = suite.client.EthSendRawTransaction(rawTx)
 	suite.Require().NotNil(err)
@@ -312,7 +333,7 @@ func (suite Model18) Test1822_SendRawTransactionWithErrorSignatureIsFail() {
 	signTx, err := types.SignTx(tx, types.NewEIP155Signer(big.NewInt(1356)), privateKey)
 	suite.Require().Nil(err)
 	data, err := signTx.MarshalBinary()
-	rawTx := common.Bytes2Hex(data)
+	rawTx := hexutil.Bytes(data).String()
 
 	_, err = suite.client.EthSendRawTransaction(rawTx)
 	suite.Require().NotNil(err)
@@ -348,7 +369,7 @@ func (suite Model18) Test1823_SendRawTransactionWithLessPriceIsFail() {
 	signTx, err := types.SignTx(tx, types.NewEIP155Signer(big.NewInt(1356)), privateKey)
 	suite.Require().Nil(err)
 	data, err := signTx.MarshalBinary()
-	rawTx := common.Bytes2Hex(data)
+	rawTx := hexutil.Bytes(data).String()
 
 	_, err = suite.client.EthSendRawTransaction(rawTx)
 	suite.Require().NotNil(err)
@@ -356,22 +377,85 @@ func (suite Model18) Test1823_SendRawTransactionWithLessPriceIsFail() {
 
 //tc：根据正确的参数调用内置合约，调用成功
 func (suite Model18) Test1824_CallIsSuccess() {
+	hash, err := suite.client.EthSendRawTransaction("0xf901c28082c3508301eaed8080b90170608060405234801561001057600080fd5b50610150806100206000396000f3fe608060405234801561001057600080fd5b50600436106100365760003560e01c80632e64cec11461003b5780636057361d14610059575b600080fd5b610043610075565b60405161005091906100d9565b60405180910390f35b610073600480360381019061006e919061009d565b61007e565b005b60008054905090565b8060008190555050565b60008135905061009781610103565b92915050565b6000602082840312156100b3576100b26100fe565b5b60006100c184828501610088565b91505092915050565b6100d3816100f4565b82525050565b60006020820190506100ee60008301846100ca565b92915050565b6000819050919050565b600080fd5b61010c816100f4565b811461011757600080fd5b5056fea26469706673582212209a159a4f3847890f10bfb87871a61eba91c5dbf5ee3cf6398207e292eee22a1664736f6c63430008070033820abca004262a31c3f0ef18950fb1e2d53d894bd2d1096a5140e3575eff53924baac054a07cbc9db70bd61fb4b6e4979b8f35a53675a745dd4dfedb595bd27fdc9aa0fb91")
+	suite.Require().Nil(err)
+	pk, from, err := repo.Node1Priv()
+	suite.Require().Nil(err)
+	client := suite.NewClient(pk)
+	res, err := client.GetReceipt(hash)
+	suite.Require().Nil(err)
 
+	// set value 100
+	_, err = suite.client.EthSendRawTransaction("0xf8870782c350825d74941dfcd0c15e69737b346a08fe3ecd1ca7d9d482bb80a46057361d0000000000000000000000000000000000000000000000000000000000000458820abca048e58c7dc5932d08ef0184b59c7b3470c45c1f32e151b20f8f12f3085e1e68aca06993ef8e147063467be9bed9aba8075922f36b378f6869d6fd03d09e6db47fb2")
+	suite.Require().Nil(err)
+
+	// get value
+	tx := ethrpc.T{
+		From: from.String(),
+		To:   res.ContractAddress.String(),
+		Data: "0x2e64cec1",
+	}
+	value, err := suite.client.EthCall(tx, "latest")
+	suite.Require().Nil(err)
+	suite.Require().Equal("0x0000000000000000000000000000000000000000000000000000000000000064", value)
 }
 
 //tc：根据错误的参数调用内置合约，调用失败
 func (suite Model18) Test1825_CallWithErrorArgsIsFail() {
+	hash, err := suite.client.EthSendRawTransaction("0xf901c28082c3508301eaed8080b90170608060405234801561001057600080fd5b50610150806100206000396000f3fe608060405234801561001057600080fd5b50600436106100365760003560e01c80632e64cec11461003b5780636057361d14610059575b600080fd5b610043610075565b60405161005091906100d9565b60405180910390f35b610073600480360381019061006e919061009d565b61007e565b005b60008054905090565b8060008190555050565b60008135905061009781610103565b92915050565b6000602082840312156100b3576100b26100fe565b5b60006100c184828501610088565b91505092915050565b6100d3816100f4565b82525050565b60006020820190506100ee60008301846100ca565b92915050565b6000819050919050565b600080fd5b61010c816100f4565b811461011757600080fd5b5056fea26469706673582212209a159a4f3847890f10bfb87871a61eba91c5dbf5ee3cf6398207e292eee22a1664736f6c63430008070033820abca004262a31c3f0ef18950fb1e2d53d894bd2d1096a5140e3575eff53924baac054a07cbc9db70bd61fb4b6e4979b8f35a53675a745dd4dfedb595bd27fdc9aa0fb91")
+	suite.Require().Nil(err)
+	pk, from, err := repo.Node1Priv()
+	suite.Require().Nil(err)
+	client := suite.NewClient(pk)
+	res, err := client.GetReceipt(hash)
+	suite.Require().Nil(err)
 
+	tx := ethrpc.T{
+		From: from.String(),
+		To:   res.ContractAddress.String(),
+		Data: "0x2e64cec2",
+	}
+	_, err = suite.client.EthCall(tx, "latest")
+	suite.Require().NotNil(err)
 }
 
 //tc：根据正确的参数获取调用内置合约的gas limit，获取成功
 func (suite Model18) Test1826_EstimateGasIsSuccess() {
+	hash, err := suite.client.EthSendRawTransaction("0xf901c28082c3508301eaed8080b90170608060405234801561001057600080fd5b50610150806100206000396000f3fe608060405234801561001057600080fd5b50600436106100365760003560e01c80632e64cec11461003b5780636057361d14610059575b600080fd5b610043610075565b60405161005091906100d9565b60405180910390f35b610073600480360381019061006e919061009d565b61007e565b005b60008054905090565b8060008190555050565b60008135905061009781610103565b92915050565b6000602082840312156100b3576100b26100fe565b5b60006100c184828501610088565b91505092915050565b6100d3816100f4565b82525050565b60006020820190506100ee60008301846100ca565b92915050565b6000819050919050565b600080fd5b61010c816100f4565b811461011757600080fd5b5056fea26469706673582212209a159a4f3847890f10bfb87871a61eba91c5dbf5ee3cf6398207e292eee22a1664736f6c63430008070033820abca004262a31c3f0ef18950fb1e2d53d894bd2d1096a5140e3575eff53924baac054a07cbc9db70bd61fb4b6e4979b8f35a53675a745dd4dfedb595bd27fdc9aa0fb91")
+	suite.Require().Nil(err)
+	pk, from, err := repo.Node1Priv()
+	suite.Require().Nil(err)
+	client := suite.NewClient(pk)
+	res, err := client.GetReceipt(hash)
+	suite.Require().Nil(err)
 
+	tx := ethrpc.T{
+		From: from.String(),
+		To:   res.ContractAddress.String(),
+		Data: "0x2e64cec1",
+	}
+	value, err := suite.client.EthEstimateGas(tx)
+	suite.Require().Nil(err)
+	suite.Require().Greater(value, 21000)
 }
 
 //tc：根据错误的参数获取调用内置合约的gas limit，获取失败
 func (suite Model18) Test1827_EstimateGasWithErrorArgsIsFall() {
+	hash, err := suite.client.EthSendRawTransaction("0xf901c28082c3508301eaed8080b90170608060405234801561001057600080fd5b50610150806100206000396000f3fe608060405234801561001057600080fd5b50600436106100365760003560e01c80632e64cec11461003b5780636057361d14610059575b600080fd5b610043610075565b60405161005091906100d9565b60405180910390f35b610073600480360381019061006e919061009d565b61007e565b005b60008054905090565b8060008190555050565b60008135905061009781610103565b92915050565b6000602082840312156100b3576100b26100fe565b5b60006100c184828501610088565b91505092915050565b6100d3816100f4565b82525050565b60006020820190506100ee60008301846100ca565b92915050565b6000819050919050565b600080fd5b61010c816100f4565b811461011757600080fd5b5056fea26469706673582212209a159a4f3847890f10bfb87871a61eba91c5dbf5ee3cf6398207e292eee22a1664736f6c63430008070033820abca004262a31c3f0ef18950fb1e2d53d894bd2d1096a5140e3575eff53924baac054a07cbc9db70bd61fb4b6e4979b8f35a53675a745dd4dfedb595bd27fdc9aa0fb91")
+	suite.Require().Nil(err)
+	pk, from, err := repo.Node1Priv()
+	suite.Require().Nil(err)
+	client := suite.NewClient(pk)
+	res, err := client.GetReceipt(hash)
+	suite.Require().Nil(err)
 
+	tx := ethrpc.T{
+		From: from.String(),
+		To:   res.ContractAddress.String(),
+		Data: "0x2e64cec2",
+	}
+	_, err = suite.client.EthEstimateGas(tx)
+	suite.Require().NotNil(err)
 }
 
 //tc：根据正确的区块hash获取区块内全部的交易hash，获取成功
@@ -487,10 +571,11 @@ func (suite Model18) Test1836_GetTransactionByHashIsSuccess() {
 	signTx, err := types.SignTx(tx, types.NewEIP155Signer(big.NewInt(1356)), privateKey)
 	suite.Require().Nil(err)
 	data, err := signTx.MarshalBinary()
-	rawTx := common.Bytes2Hex(data)
+	rawTx := hexutil.Bytes(data).String()
 
 	hash, err := suite.client.EthSendRawTransaction(rawTx)
 	suite.Require().Nil(err)
+	time.Sleep(1 * time.Second)
 	res, err := suite.client.EthGetTransactionByHash(hash)
 	suite.Require().Nil(err)
 	suite.Require().NotNil(res)
@@ -533,10 +618,11 @@ func (suite Model18) Test1838_GetTransactionByBlockHashAndIndexIsSuccess() {
 	signTx, err := types.SignTx(tx, types.NewEIP155Signer(big.NewInt(1356)), privateKey)
 	suite.Require().Nil(err)
 	data, err := signTx.MarshalBinary()
-	rawTx := common.Bytes2Hex(data)
+	rawTx := hexutil.Bytes(data).String()
 
 	hash, err := suite.client.EthSendRawTransaction(rawTx)
 	suite.Require().Nil(err)
+	time.Sleep(1 * time.Second)
 	res1, err := suite.client.EthGetTransactionByHash(hash)
 	suite.Require().Nil(err)
 	res2, err := suite.client.EthGetTransactionByBlockHashAndIndex(res1.BlockHash, *res1.TransactionIndex)
@@ -581,10 +667,11 @@ func (suite Model18) Test1840_GetTransactionByBlockHashAndIndexWithErrorIndexIsF
 	signTx, err := types.SignTx(tx, types.NewEIP155Signer(big.NewInt(1356)), privateKey)
 	suite.Require().Nil(err)
 	data, err := signTx.MarshalBinary()
-	rawTx := common.Bytes2Hex(data)
+	rawTx := hexutil.Bytes(data).String()
 
 	hash, err := suite.client.EthSendRawTransaction(rawTx)
 	suite.Require().Nil(err)
+	time.Sleep(1 * time.Second)
 	res1, err := suite.client.EthGetTransactionByHash(hash)
 	suite.Require().Nil(err)
 	_, err = suite.client.EthGetTransactionByBlockHashAndIndex(res1.BlockHash, 2001)
@@ -628,10 +715,11 @@ func (suite Model18) Test1842_GetTransactionByBlockNumberAndIndexIsSuccess() {
 	signTx, err := types.SignTx(tx, types.NewEIP155Signer(big.NewInt(1356)), privateKey)
 	suite.Require().Nil(err)
 	data, err := signTx.MarshalBinary()
-	rawTx := common.Bytes2Hex(data)
+	rawTx := hexutil.Bytes(data).String()
 
 	hash, err := suite.client.EthSendRawTransaction(rawTx)
 	suite.Require().Nil(err)
+	time.Sleep(1 * time.Second)
 	res1, err := suite.client.EthGetTransactionByHash(hash)
 	suite.Require().Nil(err)
 	res2, err := suite.client.EthGetTransactionByBlockNumberAndIndex(*res1.BlockNumber, *res1.TransactionIndex)
@@ -681,10 +769,11 @@ func (suite Model18) Test1844_GetTransactionByBlockNumberAndIndexWithErrorIndexI
 	signTx, err := types.SignTx(tx, types.NewEIP155Signer(big.NewInt(1356)), privateKey)
 	suite.Require().Nil(err)
 	data, err := signTx.MarshalBinary()
-	rawTx := common.Bytes2Hex(data)
+	rawTx := hexutil.Bytes(data).String()
 
 	hash, err := suite.client.EthSendRawTransaction(rawTx)
 	suite.Require().Nil(err)
+	time.Sleep(1 * time.Second)
 	res1, err := suite.client.EthGetTransactionByHash(hash)
 	suite.Require().Nil(err)
 	_, err = suite.client.EthGetTransactionByBlockNumberAndIndex(*res1.BlockNumber, 2001)
@@ -733,7 +822,7 @@ func (suite Model18) Test1846_GetTransactionReceiptIsSuccess() {
 	signTx, err := types.SignTx(tx, types.NewEIP155Signer(big.NewInt(1356)), privateKey)
 	suite.Require().Nil(err)
 	data, err := signTx.MarshalBinary()
-	rawTx := common.Bytes2Hex(data)
+	rawTx := hexutil.Bytes(data).String()
 
 	hash, err := suite.client.EthSendRawTransaction(rawTx)
 	suite.Require().Nil(err)
